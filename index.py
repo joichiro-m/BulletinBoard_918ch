a65bd93342d5fbc5ef28d231a145468c48309741
joichiro-m <joichiro.miyazaki@crane-i.co.jp>
2021-05-25 17:25:55 -0700

    一言掲示板

---
+++ index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print ('Content-type: text/html\n')
+print ("""
+
+<!DOCTYPE html>
+<html>
+	<head><title>CGItest</title></head>
+	<body>Hello,world!</dody>
+</html>	
+
+""")
